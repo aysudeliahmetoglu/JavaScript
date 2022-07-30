9994ff0242dbaedc9c8eb7a2dbd43170a4c7f7cb
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2022-07-30 15:32:07 +0300

    variables

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
-    console.log("hello");
-    console.log(true);
-    console.log("1234");
+    // console.log("hello");
+    // console.log(true);
+    // console.log("1234");
+
+    //variable definiton
+    var name;
+    var name_surname;
+    var nameSurname;
+    // var Let const
+    var x="Name";
+    console.log(x);
+    let y='surname';
+    console.log(y);
+    const email='asc@gmail.com'
+    console.log(email);
+    // email='asc1@gmail.com'   - error -
+    console.log(email);
+    // Variable Types
+    //Primitive Types
+    //undefined
+    var x;
+    console.log(typeof x); //undefined
+    //null
+    var y=null;
+    console.log(typeof y);  //object
+    //string
+    var firstname='Aysu';
+    console.log(typeof firstname);
+    //number
+    var age=21;
+    console.log(typeof age);
+    //boolean
+    var online=true
+    console.log(typeof online);
+
+
+    //Reference Types 
+    //array
+    var colors=['red','blue','pink','yellow']
+    
+    
+    //objects
+
+    var person={
+        name:'Sam',
+        age:25
+    }
+
+    //functions
+    var isOnline=function()
+    {
+        return 0;
+    }
+    console.log(typeof isOnline);
+
+    
