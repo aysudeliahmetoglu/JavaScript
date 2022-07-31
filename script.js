5bc3a3a78698cf9fe6a0fe29374da43f12336838
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2022-07-31 15:36:20 +0300

    operators

--- script.js
+++ script.js
@@ -3,54 +3,100 @@
     // console.log("1234");
 
     //variable definiton
-    var name;
-    var name_surname;
-    var nameSurname;
+    // var name;
+    // var name_surname;
+    // var nameSurname;
     // var Let const
-    var x="Name";
-    console.log(x);
-    let y='surname';
-    console.log(y);
-    const email='asc@gmail.com'
-    console.log(email);
+    // var x="Name";
+    // console.log(x);
+    // let y='surname';
+    // console.log(y);
+    // const email='asc@gmail.com'
+    // console.log(email);
     // email='asc1@gmail.com'   - error -
-    console.log(email);
+    // console.log(email);
     // Variable Types
     //Primitive Types
-    //undefined
-    var x;
-    console.log(typeof x); //undefined
-    //null
-    var y=null;
-    console.log(typeof y);  //object
-    //string
-    var firstname='Aysu';
-    console.log(typeof firstname);
-    //number
-    var age=21;
-    console.log(typeof age);
-    //boolean
-    var online=true
-    console.log(typeof online);
-
+    //-undefined
+    // var x;
+    // console.log(typeof x); //undefined
+    //-null
+    // var y=null;
+    // console.log(typeof y);  //object
+    //-string
+    // var firstname='Aysu';
+    // console.log(typeof firstname);
+    //-number
+    // var age=21;
+    // console.log(typeof age);
+    //-boolean
+    // var online=true
+    // console.log(typeof online);
 
     //Reference Types 
-    //array
-    var colors=['red','blue','pink','yellow']
-    
-    
-    //objects
+    //-array
+    // var colors=['red','blue','pink','yellow']
+  
+    //-objects
+    // var person={
+    //     name:'Sam',
+    //     age:25
+    // }
 
-    var person={
-        name:'Sam',
-        age:25
-    }
+    //-functions
+    // var isOnline=function()
+    // {
+    //     return 0;
+    // }
+    // console.log(typeof isOnline);
 
-    //functions
-    var isOnline=function()
-    {
-        return 0;
-    }
-    console.log(typeof isOnline);
+    //Operators
+    var result;
+    const x = 60;
+    const y = 12;
+    let z = 13;
+    let t = 13;
+    //-Arithmetik operators
+    result= x + y;
+    result = x - y;
+    result = x * y;
+    result = x / y;
+    result = x % y;
+    result = x % z;
+    result = z++; //increases the number keeps it in memory
+    result = ++z; //increments the number and also prints it to the console
+    result = z--;
+    result = --z;
+    console.log(result);
+    //-Assignment Operators
 
+    result += x;
+    result -=x;
+    result /=x;
+    result %=x;
+
+    console.log(result);
+    //-Comparison Operators
+    result = x==y;   //true or false 
+    console.log(result);
+    result = z==t;
+    result = z===t; //compare typeof
+    result = x!=y;
+    result = z!=t;
+    result = z==t;
+    result = z < t;
+    result = z >= t;
+    result = z <= t;
+    console.log(result);
+    //-Logic Operators
+    //&& (and )
+    result = (x > t) && (y < z)
+    console.log(result);
+    // ||(or )
+    result = (x = t) || (x < z)
+
+    // !(not )
+    result =!(z > y)
+
+   
     
